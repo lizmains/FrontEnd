768494140e4196ddb0396c16866f630e68e69dd0
Liz <emains@ycp.edu>
2024-03-25 00:55:30 -0400

    started working on game logic, buttons change color, have live score updates started.

--- MauiApp2/GamePage.xaml.cs
+++ MauiApp2/GamePage.xaml.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// using UIKit;
+using System.Windows.Input;
 
 namespace MauiApp2;
 
 public partial class GamePage : ContentPage
 {
-    public List<List<string>> ButtonItems { get; set; }
+    public List<Boolean> pins = new List<Boolean>(10);
+    public List<int> frames = new List<int>(10);
+    public List<int> shots = new List<int>(21);
+    private int frameNum = 0;
+    private int shotNum = 0;
+    public int gameNum = 1;
+    public int score = 0;
+    public int scoreNum = 10;
+    
     public GamePage()
     {
         InitializeComponent();
 
-        ButtonItems = new List<List<string>>
+        for (int i = 0; i < 10; i++)
         {
-            new List<string> { "Pin 7", "Pin 8", "Pin 9", "Pin 10" },
-            new List<string> { "Pin 4", "Pin 5", "Pin 6" },
-            new List<string> { "Pin 2", "Pin 3" },
-            new List<string> { "Pin 1" }
-        };
-        // DataContext = this;
+            pins.Add(false);
+            //KNOCKED DOWN = FALSE
+        }
+        
     }
     
     private async void OnNewGameBtnClicked(object sender, EventArgs e) //navigate to simulator
@@ -31,21 +37,53 @@ public partial class GamePage : ContentPage
 
     private async void OnPinBtnClicked(object sender, EventArgs e)
     {
-        //thing happens here
+        var button = (Button)sender;
+        int pin = int.Parse(button.Text);
+        
+        int frameScore = 10;
+        pins[pin] = !pins[pin];
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (pins[i])
+            {
+                frameScore--;
+                
+            }
+        }
+
+        if (button.BackgroundColor == Colors.DarkOrchid)
+        {
+            button.BackgroundColor = Colors.Teal;
+        }
+        else
+        {
+            button.BackgroundColor = Colors.DarkOrchid;
+        }
+
+        scoreNum = frameScore;
+        score += scoreNum;
+        UpdateScoreDisplay();
+
     }
+
+    void UpdateScoreDisplay()
+    {
+        FrameP1.Text = $"{scoreNum}";
+    }
+    
     
     private async void OnGutterballBtnClicked(object sender, EventArgs e) //navigate to simulator
     {
-        // go to next shot in frame OR next frame
-        //no pins fall
-        //score += 0
+        frameNum++;
+        shotNum++;
     }
     
     private async void OnFoulBtnClicked(object sender, EventArgs e) //navigate to simulator
     {
-        // go to next shot in frame OR next frame
-        //no pins fall
         //score += 0
+        //still have to calculate how many pins were knocked down, just doesn;'t contribute to score
+        //can spare off a foul
         //idk what fouls mean in bowling
     }
     
@@ -62,18 +100,14 @@ public partial class GamePage : ContentPage
         //all pins fall
         //score += whatevers left over from first shot
     }
-    
+
     private async void OnPrevFrameBtnClicked(object sender, EventArgs e) //navigate to simulator
     {
-        // go to next frame
-        //all pins fall
-        //score += whatevers left over from first shot
+        frameNum--;
     }
     
     private async void OnNextFrameBtnClicked(object sender, EventArgs e) //navigate to simulator
     {
-        // go to next frame
-        //all pins fall
-        //score += whatevers left over from first shot
+        frameNum++;
     }
 }
