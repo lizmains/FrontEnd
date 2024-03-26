fb1c114afa98b26878a08b1fa36b70f8168d1c3e
Liz <emains@ycp.edu>
2024-03-25 23:27:00 -0400

    started setting up backend for game logic - have to reorganize GamePage to better use MVVM architecture

---
+++ MauiApp2/BowlingFrame.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp2;
+
+public class BowlingFrame
+{
+    public int firstRoll { get; set; }
+    public int secondRoll { get; set; }
+    public int frameScore { get; set; }
+    //add other shit in here later for things like 10th frame
+}
---
+++ MauiApp2/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp2;
+
+public class BowlingGame
+{
+    public List<BowlingFrame> Frames { get; set; }
+
+    public BowlingGame()
+    {
+        Frames = new List<BowlingFrame>(10);
+        InitializeFrames();
+    }
+
+    private void InitializeFrames()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Frames.Add(new BowlingFrame());
+        }
+    }
+    
+    //calculate shit in here later also for scores and rolls
+}
